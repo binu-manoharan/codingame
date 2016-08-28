f8ff1799730f8db73c5ea165b242777de963ed41
binu manoharan <binu1988@gmail.com>
2016-08-28 12:50:36 +0100

    Improve test method name.

--- src/test/java/MountainHelperTest.java
+++ src/test/java/MountainHelperTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
  */
 public class MountainHelperTest {
     @Test
-    public void should_compile() throws Exception {
+    public void should_give_the_highest_mountain_index() throws Exception {
         final MountainHelper mountainHelper = new MountainHelper();
         final int mountains[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
