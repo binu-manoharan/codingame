8bc713c812279d93321021fd515cacadc7bb11da
Revant Cherian Idikulay <revantcherian@gmail.com>
2016-09-03 18:27:37 +0530

    Fixed compilation failure - finished moutainerHelper method

--- src/main/java/MountainHelper.java
+++ src/main/java/MountainHelper.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Helper class to work with mountain heights.
  */
 public class MountainHelper {
 
-    public void getHighestMountain(int[] mountains) {
-
+    public int getHighestMountain(int[] mountains) {
+        return IntStream.range(0,mountains.length)
+                .reduce((i,j) -> mountains[i] < mountains[j] ? j : i)
+                .getAsInt();
     }
 }
