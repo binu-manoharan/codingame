2c1a81664bd828a5f2e39cfb30e70888ee0f7a28
binu manoharan <binu1988@gmail.com>
2016-09-03 13:38:09 +0100

    Add assertions to MountainHelperTest.

--- src/test/java/MountainHelperTest.java
+++ src/test/java/MountainHelperTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Test for {@link MountainHelper}
  */
@@ -9,6 +12,7 @@ public class MountainHelperTest {
         final MountainHelper mountainHelper = new MountainHelper();
         final int mountains[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        mountainHelper.getHighestMountain(mountains);
+        int indexOfHighestMountain = mountainHelper.getHighestMountain(mountains);
+        assertThat("Highest mountain is the 8th one.", indexOfHighestMountain, is(7));
     }
 }
