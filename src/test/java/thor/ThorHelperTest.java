a7928e05415ba38543301c2381486026b3bf1a5f
binu manoharan <binu1988@gmail.com>
2016-09-03 15:28:58 +0100

    Add Tests for Thor's north and east directions.

--- src/test/java/thor/ThorHelperTest.java
+++ src/test/java/thor/ThorHelperTest.java
@@ -1,5 +1,7 @@
 package thor;
 
+import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -10,16 +12,41 @@ import static org.junit.Assert.*;
  * Test for {@link ThorHelper}
  */
 public class ThorHelperTest {
+
+    private int lightX =  10;
+    private int lightY = 10;
+    private int thorX = 10;
+    private int thorY = 10;
+
     @Test
     public void thor_should_go_west() throws Exception {
         ThorHelper thorHelper = new ThorHelper();
-        final int lightX = 10;
-        final int lightY = 10;
-        final int thorX = 12;
-        final int thorY = 10;
+        thorX = 12;
 
         String direction = thorHelper.getNextDirection(lightX, lightY, thorX, thorY);
-        assertThat("Thor should go west", direction, is(notNullValue()));
-        assertThat("Thor should go west", direction, is("W"));
+        assertDirection(direction, "Thor should go west", is("W"));
+    }
+
+    @Test
+    public void thor_should_go_east() throws Exception {
+        ThorHelper thorHelper = new ThorHelper();
+        lightX = 12;
+
+        String direction = thorHelper.getNextDirection(lightX, lightY, thorX, thorY);
+        assertDirection(direction, "Thor should go east", is("E"));
+    }
+
+    @Test
+    public void thor_should_go_north() throws Exception {
+        ThorHelper thorHelper = new ThorHelper();
+        thorY = 8;
+
+        String direction = thorHelper.getNextDirection(lightX, lightY, thorX, thorY);
+        assertDirection(direction, "Thor should go north", is("N"));
+    }
+
+    private void assertDirection(String direction, String reason, Matcher<String> e) {
+        assertThat(reason, direction, is(notNullValue()));
+        assertThat(reason, direction, e);
     }
 }
