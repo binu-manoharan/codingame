dbe62d3be90353085d41c6ba8d3e23fde66f3d47
binu manoharan <binu1988@gmail.com>
2016-09-11 17:54:47 +0100

    Add skeletal structure for merge script.

---
+++ src/main/java/org/codingame/utils/MergeFiles.java
@@ -0,0 +1,10 @@
+package org.codingame.utils;
+
+/**
+ * Merge files with player template for submission on coding game website.
+ */
+public class MergeFiles {
+    public static void main(String[] args) {
+        //TODO Add TODOs
+    }
+}
